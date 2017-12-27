2b33907560a3f32afcef7cb772f8665f67fba1b1
Forest Hoffman <foresthoffman@users.noreply.github.com>
2017-12-26 16:36:06 -0800

    Init repo

---
+++ main.go
@@ -0,0 +1,28 @@
+/**
+ * main.go
+ *
+ * Copyright (c) 2017 Forest Hoffman. All Rights Reserved.
+ * License: MIT License (see the included LICENSE file)
+ */
+
+package main
+
+import (
+	"github.com/foresthoffman/twitchbot"
+	"time"
+)
+
+func main() {
+
+	// Replace the channel name, bot name, and the path to the private directory with your respective
+	// values.
+	myBot := twitchbot.BasicBot{
+		Channel:     "twitch",
+		MsgRate:     time.Duration(20/30) * time.Millisecond,
+		Name:        "TwitchBot",
+		Port:        "6667",
+		PrivatePath: "./private/oauth.json",
+		Server:      "irc.chat.twitch.tv",
+	}
+	myBot.Start()
+}
